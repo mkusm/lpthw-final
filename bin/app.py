7b8226669c4a84b6a959cb909097ab37de44dc05
Michał Kuśmierczyk <mkus92@gmail.com>
2015-04-05 23:40:31 +0200

    app.py from the lpthw book

--- bin/app.py
+++ bin/app.py
@@ -1,23 +1,42 @@
 # -*- coding:utf-8 -*-
 
 import web
+from gothonweb import map
 
 urls = (
-    '/hello', 'Index'
+    '/game', 'GameEngine',
+    '/', 'Index'
 )
 
 app = web.application(urls, globals())
 
+# little hack so that debug mode workds with sessions
+if web.config.get('_session') is None:
+    store = web.session.DiskStore('sessions')
+    session = web.session.Session(app, store, initializer=['room': None])
+    web.config._session = session
+else:
+    session = web.config._session
+
 render = web.template.render('templates/', base="layout")
 
-class Index:
+class GameEngine(object):
+
     def GET(self):
-        return render.hello_form()
+        if session.room:
+            return render.show_room(room=session.room)
+        else:
+            # why is there here? do you need it?
+            return render.hello_form()
 
     def POST(self):
-        form = web.input(name="Michał", greet="Hola")
-        greeting = "%s, %s" % (form.greet, form.name)
-        return render.index(greeting = greeting)
+        form = web.input(action=None)
+
+        # there is a bug here, can you fix it?
+        if session.room and form.action:
+            session.room = session.room.go(form.action)
+
+        web.seeother("/game")
 
 if __name__ == "__main__":
     app.run()
