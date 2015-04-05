ef62e52345d5c0e8758047d96b87884a45391a19
Michał Kuśmierczyk <mkus92@gmail.com>
2015-04-05 22:00:16 +0200

    Init commit. Basic map, tests and web setup.

---
+++ bin/app.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+import web
+
+urls = (
+    '/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', base="layout")
+
+class Index:
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Michał", greet="Hola")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
