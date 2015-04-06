a5d43c8792cc25818cfd9b94bba192a8e27296e9
Michał Kuśmierczyk <kusm@protonmail.ch>
2015-04-06 19:39:08 +0200

    Clean up app_tests.py, add check for /

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -5,19 +5,8 @@ from bin.app import app
 from tests.tools import assert_response
 
 def test_index():
-    # check that we get a 404 on the / URL
     resp = app.request("/game")
     assert_response(resp)
 
-    # test our first GET request to /hello
-    resp = app.request("/hello")
-    assert_response(resp, status="404")
-
-    # make sure default values work for the form
-    # resp = app.request("/hello", method="POST")
-    # assert_response(resp, contains="Michał")
-
-    # test that we get expected values
-    # data = {'name': 'Zed', 'greet': 'Hola'}
-    # resp = app.request("/hello", method="POST", data=data)
-    # assert_response(resp, contains="Zed")
+    resp = app.request("/")
+    assert_response(resp, status="303 See Other")
